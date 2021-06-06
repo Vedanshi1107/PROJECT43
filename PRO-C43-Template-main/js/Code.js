599d2e0470291f33e109fc499b1c8318def20d3c
Vedanshi1107 <2335.stkabirnrnp@gmail.com>
2021-06-06 14:50:08 +0530

    first commit

---
+++ PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,60)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,80)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("N U I F O C N T", 700,160)
+    fill("lightblue")
+    text("Hint: We need it to run the code !!", 700,180)
+    
+    fill("white")
+    textSize(15)
+    text("S A D T A E B A", 100,300)
+    fill("lightblue")
+    text("Hint: Add in index.html !!", 100,320)
+}
